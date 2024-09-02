6c5846524dd2bc3d0d2a8cfee816d562a0de3558
YouD0313 <102004480+YouD0313@users.noreply.github.com>
2024-09-02 22:55:56 +0900

    depoly dist change

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,6 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [react()],
-	push: { branches: ['main'] },
 	build: {
 		minify: false,
 		chunkSizeWarningLimit: 1600,
